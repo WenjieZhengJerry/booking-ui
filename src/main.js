41daef7c36dba3bec7b7469873c17dcbad8f3f6c
WenjieZhengJerry <2388623483@qq.com>
2019-12-06 17:26:45 +0800

    订单管理连接后台成功

--- src/main.js
+++ src/main.js
@@ -4,13 +4,22 @@ import Vue from 'vue'
 import Antd from 'ant-design-vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import 'ant-design-vue/dist/antd.css'
 
 import { Button } from 'ant-design-vue';
+import { VueAxios } from './utils/request'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios //将axios挂载在Vue实例原型上
 Vue.use(Antd)
 Vue.use(Button);
+Vue.use(VueAxios)
+
+// 设置axios请求的token
+axios.defaults.headers.common['token'] = 'f4c902c9ae5a2a9d8f84868ad064e706'
+//设置请求头
+axios.defaults.headers.post["Content-type"] = "application/json"
 
 /* eslint-disable no-new */
 new Vue({
--- src/utils/pageable.js
+++ src/utils/pageable.js
@@ -23,29 +23,12 @@ export function toOffsetParam (params) {
   
   export function parsePage (params) {
     if (params === undefined) {
-			const tempData = []
-			for (let i = 1; i <= 10; ++i) {
-				let status = ''
-				if (i % 5 == 0) status = 'FINISH'
-				else if (i % 4 == 0) status = 'UNPAY'
-				else if (i % 3 == 0) status = 'CANCEL'
-				else status = 'UNUSE'
-				tempData.push({
-					id: i,
-					orderNumber: '12306',
-					hotel: '维也纳国际酒店',
-					uName: '小泽又沐风',
-					amount: '299.9',
-					orderStatus: status,
-					createTime: '2019-12-5 16:26:37'
-				});
-			}
       return {
         pageSize: 10,
         pageNo: 1,
-        totalCount: 10,
+        totalCount: 0,
         totalPage: 1,
-        data: tempData
+        data: []
       }
     }
     const size = params.data.size
